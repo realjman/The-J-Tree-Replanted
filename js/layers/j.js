02e88fb14b24679c0693a4056be444c713dbfaca
realjman <ongjoel080808@gmail.com>
2024-09-01 17:15:53 +0800

    v0.0.1c

--- js/layers/j.js
+++ js/layers/j.js
@@ -11,7 +11,7 @@ addLayer("j", {
     color: "#aba",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "J-points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "J-fragments", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.4, // Prestige currency exponent
